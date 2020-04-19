c037fdc788fe63f77804dbc122fe95c2bc4f38fa
Hadik <hadikcze@gmail.com>
2020-04-19 21:08:26 +0200

    Intervals

--- src/core/shelfs/AbstractShelf.ts
+++ src/core/shelfs/AbstractShelf.ts
@@ -1,15 +1,22 @@
 import GameScene from "scenes/GameScene";
 import {ShelfState} from "enums/ShelfState";
-import Image = Phaser.GameObjects.Image;
 import {Shelfs} from "enums/Shelfs";
 import {Depths} from "enums/Depths";
 import WorldEnvironment from "core/WorldEnvironment";
 import Highlightable from "core/Highlightable";
 import {AnimalConfig} from "types/AnimalConfig";
+import NumberHelpers from "helpers/NumberHelpers";
+import Image = Phaser.GameObjects.Image;
+import EventEmitter = Phaser.Events.EventEmitter;
 
 export default abstract class AbstractShelf extends Phaser.GameObjects.Container {
 
+    public static readonly NEED_FOOD: 'need_food';
+    public static readonly NEED_CLEAN_POO: 'need_clean_poo';
+    public static readonly DIE: 'die';
+
     public readonly highlight: Highlightable;
+    protected events: EventEmitter;
     protected scene: GameScene;
     protected shelfType: Shelfs;
     protected lives: number = 10;
@@ -28,6 +35,7 @@ export default abstract class AbstractShelf extends Phaser.GameObjects.Container
         this.shelfType = shelfType;
         this.title = title;
         this.config = config;
+        this.events = new EventEmitter();
         this.highlight = new Highlightable(scene, this, title);
 
         this.scene.add.existing(this);
@@ -44,6 +52,9 @@ export default abstract class AbstractShelf extends Phaser.GameObjects.Container
         this.animalImage = this.scene.add.image(-10000, -10000, '');
 
         this.setInteractive();
+
+        this.startFeedInterval();
+        this.startPooInterval();
     }
 
     hideAnimal (): void {
@@ -54,5 +65,31 @@ export default abstract class AbstractShelf extends Phaser.GameObjects.Container
     showAnimal (): void {
         this.animalImage.setVisible(true);
         this.animalImage2?.setVisible(true);
+    }
+
+    private startFeedInterval (): void {
+        if (this.shelfState === ShelfState.WAITING_FOR_FEED) return this.startFeedInterval();
+
+        this.scene.time.addEvent({
+            delay: NumberHelpers.randomIntInRange(this.config?.feedDuration[0], this.config?.feedDuration[1]) * 1000,
+            callbackScope: this,
+            callback: () => {
+                this.events.emit(AbstractShelf.NEED_FOOD);
+                this.startFeedInterval();
+            }
+        });
+    }
+
+    private startPooInterval (): void {
+        if (this.shelfState === ShelfState.WAITING_FOR_CLEAN) return this.startPooInterval();
+
+        this.scene.time.addEvent({
+            delay: NumberHelpers.randomIntInRange(this.config?.pooDuration[0], this.config?.pooDuration[1]) * 1000,
+            callbackScope: this,
+            callback: () => {
+                this.events.emit(AbstractShelf.NEED_CLEAN_POO);
+                this.startPooInterval();
+            }
+        });
     }
 }
