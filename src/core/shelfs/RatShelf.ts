d6181692ec6a5c6b5a3be63a4c97601be6a0054f
Hadik <hadikcze@gmail.com>
2020-04-19 18:54:11 +0200

    Rat animation

--- src/core/shelfs/RatShelf.ts
+++ src/core/shelfs/RatShelf.ts
@@ -4,6 +4,9 @@ import {Shelfs} from "enums/Shelfs";
 
 export default class RatShelf extends AbstractShelf {
 
+    private previousX: number = 0;
+    private previous2X: number = 0;
+
     constructor (scene: GameScene, x: number, y: number) {
         super(scene, x, y, Shelfs.RAT);
 
@@ -15,5 +18,44 @@ export default class RatShelf extends AbstractShelf {
 
         this.cage = this.scene.add.image(42, -59, 'assets', 'game_rat_cage');
         this.add(this.cage);
+
+        let duration = 5000;
+        this.scene.add.tween({
+            targets: this.animalImage,
+            x: 55,
+            duration: duration,
+            yoyo: true,
+            repeatDelay: Phaser.Math.RND.between(450, 750),
+            loopDelay: Phaser.Math.RND.between(450, 500),
+            delay: Phaser.Math.RND.between(0, 350),
+            repeat: Infinity
+        });
+
+        this.scene.add.tween({
+            targets: this.animalImage2,
+            x: 25,
+            duration: duration,
+            yoyo: true,
+            repeatDelay: Phaser.Math.RND.between(450, 750),
+            loopDelay: Phaser.Math.RND.between(450, 500),
+            delay: Phaser.Math.RND.between(1000, 2500),
+            repeat: Infinity
+        });
+    }
+
+    preUpdate (): void {
+        if (this.previousX < this.animalImage.x) {
+            this.animalImage.setScale(1, 1);
+        } else {
+            this.animalImage.setScale(-1, 1);
+        }
+        this.previousX = this.animalImage.x;
+
+        if (this.previous2X < this.animalImage2.x) {
+            this.animalImage2.setScale(-1, 1);
+        } else {
+            this.animalImage2.setScale(1, 1);
+        }
+        this.previous2X = this.animalImage2.x;
     }
 }
