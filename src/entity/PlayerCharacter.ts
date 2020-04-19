887d9e0f1f5133ff6255a0e9ba7a504e17aa3ed0
Hadik <hadikcze@gmail.com>
2020-04-19 14:35:57 +0200

    Player character shadow

--- src/entity/PlayerCharacter.ts
+++ src/entity/PlayerCharacter.ts
@@ -3,6 +3,7 @@ import GameScene from "scenes/GameScene";
 import Vector2 = Phaser.Math.Vector2;
 import EasyStarWrapper from "core/pathfinding/EasyStarWrapper";
 import {Depths} from "structs/Depths";
+import Tween = Phaser.Tweens.Tween;
 
 export default class PlayerCharacter extends Phaser.GameObjects.Container implements Phaser.GameObjects.GameObject {
 
@@ -10,9 +11,11 @@ export default class PlayerCharacter extends Phaser.GameObjects.Container implem
     public debugPath: boolean = sessionStorage.getItem('debugPath') && JSON.parse(sessionStorage.getItem('debugPath') as string) ? true : false;
     private static readonly SCALE: number = 3;
     private pathfinding: EasyStarWrapper;
+    private shadow: Phaser.GameObjects.Graphics;
     private character: Phaser.GameObjects.Sprite;
     private positionToMove: Vector2 | null = null;
     private path: Vector2[] = [];
+    private shadowAnimation: Tween;
 
     constructor(scene: GameScene, x: number, y: number, pathfinding: EasyStarWrapper) {
         super(scene, x, y, []);
@@ -33,6 +36,20 @@ export default class PlayerCharacter extends Phaser.GameObjects.Container implem
             frameRate: 8,
             repeat: -1
         });
+
+        this.shadow = this.scene.add.graphics({x: 0, y: 0});
+        this.shadow.fillStyle(0x000, 0.4);
+        this.shadow.fillEllipse(0, 0, 16, 8);
+        this.add(this.shadow);
+
+        this.shadowAnimation = this.scene.tweens.add({
+            targets: this.shadow,
+            scaleX: 0.6,
+            scaleY: 0.6,
+            yoyo: true,
+            repeat: Infinity,
+            duration: 200
+        }).pause();
 
         // y+5 because frame feets are not on the bottom
         this.character = this.scene.add.sprite(0, 0, 'characters', 0)
@@ -62,9 +79,11 @@ export default class PlayerCharacter extends Phaser.GameObjects.Container implem
 
         if (Math.abs(body.velocity.x) !== 0 || Math.abs(body.velocity.y) !== 0) {
             this.character.anims.play('playerCharacterWalk', true);
+            // this.shadowAnimation.play();
         } else if (Math.abs(body.velocity.x) === 0 && Math.abs(body.velocity.y) === 0) {
             this.character.setFrame(0);
             this.character.anims.stop();
+            // this.shadowAnimation.stop();
         }
 
         if (this.path.length > 0) {
@@ -78,9 +97,14 @@ export default class PlayerCharacter extends Phaser.GameObjects.Container implem
             body.setVelocity(0, 0);
         }
 
+        this.shadow.setVisible(true);
         // check depth against other things
         if (this.y <= 250) {
+            if (this.x <= 364) {
+                this.shadow.setVisible(false);
+            }
             this.setDepth(Depths.CHARACTER_UNDER_DESK);
+
         } else if (this.y <= 398) {
             this.setDepth(Depths.CHARACTER_UNDER_SHELF);
         } else {
