1c098ba3bef05678c01ab2d9c94a3dec0884834e
Hadik <hadikcze@gmail.com>
2020-04-19 18:26:39 +0200

    Spider and fish animation

--- src/core/shelfs/FishShelf.ts
+++ src/core/shelfs/FishShelf.ts
@@ -1,18 +1,41 @@
 import AbstractShelf from "core/shelfs/AbstractShelf";
 import GameScene from "scenes/GameScene";
 import {Shelfs} from "enums/Shelfs";
+import NumberHelpers from "helpers/NumberHelpers";
 
 export default class FishShelf extends AbstractShelf {
 
     constructor (scene: GameScene, x: number, y: number) {
         super(scene, x, y, Shelfs.FISH);
 
-        this.animalImage = this.scene.add.image(25, -52, 'assets', 'game_fish_orange');
+        let rangeX = [20, 65];
+        let rangeY = [-57, -48];
+        this.animalImage = this.scene.add.image(NumberHelpers.randomIntInRange(rangeX[0], rangeX[1]), NumberHelpers.randomIntInRange(rangeY[0], rangeY[1]), 'assets', 'game_fish_orange');
         this.add(this.animalImage);
-        this.animalImage2 = this.scene.add.image(50, -52, 'assets', 'game_fish_red');
+        this.animalImage2 = this.scene.add.image(NumberHelpers.randomIntInRange(rangeX[0], rangeX[1]), NumberHelpers.randomIntInRange(rangeY[0], rangeY[1]), 'assets', 'game_fish_red');
         this.add(this.animalImage2);
 
         this.cage = this.scene.add.image(41, -53, 'assets', 'game_fishes_aquarium_glass');
         this.add(this.cage);
+
+        let duration = 5000;
+
+        this.scene.add.tween({
+            targets: this.animalImage,
+            x: NumberHelpers.randomIntInRange(rangeX[0], rangeX[1]),
+            y: NumberHelpers.randomIntInRange(rangeY[0], rangeY[1]),
+            duration: duration,
+            yoyo: true,
+            repeat: Infinity
+        });
+
+        this.scene.add.tween({
+            targets: this.animalImage2,
+            x: NumberHelpers.randomIntInRange(rangeX[0], rangeX[1]),
+            y: NumberHelpers.randomIntInRange(rangeY[0], rangeY[1]),
+            duration: duration,
+            yoyo: true,
+            repeat: Infinity
+        });
     }
 }
--- src/core/shelfs/SpiderShelf.ts
+++ src/core/shelfs/SpiderShelf.ts
@@ -7,10 +7,18 @@ export default class SpiderShelf extends AbstractShelf {
     constructor (scene: GameScene, x: number, y: number) {
         super(scene, x, y, Shelfs.SPIDER);
 
-        this.animalImage = this.scene.add.image(37, -49, 'assets', 'game_spider');
+        this.animalImage = this.scene.add.image(70, -49, 'assets', 'game_spider');
         this.add(this.animalImage);
 
         this.cage = this.scene.add.image(45, -56, 'assets', 'game_spider_aquarium_glass');
         this.add(this.cage);
+
+        this.scene.add.tween({
+            targets: this.animalImage,
+            x: 25,
+            duration: 5000,
+            yoyo: true,
+            repeat: Infinity
+        });
     }
 }
