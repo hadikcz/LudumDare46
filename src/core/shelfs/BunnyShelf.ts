2ba4b369f3a85c05f75986e02bf19c0029247b75
Hadik <hadikcze@gmail.com>
2020-04-19 18:50:18 +0200

    Bunny animation

--- src/core/shelfs/BunnyShelf.ts
+++ src/core/shelfs/BunnyShelf.ts
@@ -7,10 +7,19 @@ export default class BunnyShelf extends AbstractShelf {
     constructor (scene: GameScene, x: number, y: number) {
         super(scene, x, y, Shelfs.BUNNY);
 
-        this.animalImage = this.scene.add.image(85, -60, 'assets', 'game_bunny');
+        this.animalImage = this.scene.add.image(81, -60, 'assets', 'game_bunny').setScale(0.86);
         this.add(this.animalImage);
 
         this.cage = this.scene.add.image(60, -75, 'assets', 'game_bunny_cage');
         this.add(this.cage);
+
+        this.scene.add.tween({
+            targets: this.animalImage,
+            angle: -Phaser.Math.RND.between(10, 15),
+            duration: Phaser.Math.RND.between(1000, 1500),
+            delay: Phaser.Math.RND.between(1000, 3000),
+            yoyo: true,
+            repeat: Infinity
+        });
     }
 }
